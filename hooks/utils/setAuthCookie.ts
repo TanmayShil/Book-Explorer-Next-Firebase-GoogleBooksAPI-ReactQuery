b48e6789052c9929600ec5e794d04d2d75bcf65b
TanmayShil <tanmayshil2002@gmail.com>
2025-07-10 16:56:00 +0530

    completed login, signup page completed using firebase also handle 404 page using lottie-react

---
+++ hooks/utils/setAuthCookie.ts
@@ -0,0 +1,12 @@
+import { setCookie, destroyCookie } from 'nookies';
+
+export const setAuthCookie = (token: string) => {
+  setCookie(null, 'token', token, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
+  });
+};
+
+export const clearAuthCookie = () => {
+  destroyCookie(null, 'token');
+};
